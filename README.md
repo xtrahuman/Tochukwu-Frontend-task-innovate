179502c0aa1812c368f22f3f973df49313ef5d2c
xtrahuman <okpara_tochukwu@yahoo.com>
2022-06-20 11:16:55 +0100

    added new icon for all interactions

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Tochukwu-Frontend-task-innovate
-This is a frontend task for innovate
+
+## Description
+> This is a frontend task for innovate
+
+
+## Built With
+
+- jsx
+- css
+- react
+- redux
+
+## Procedure to clone project
+- open your terminal
+- type git clone https://github.com/xtrahuman/tochukwu-frontend-task-innovate.git
+- type cd tochukwu-frontend-task-innovate to change directory to the folder of the project
+- in your terminal npm install to install app dependencies
+- run npm start in your terminal 
+
+
+## project link
+kindly click the link below to view the website
+- website link: [upcoming]()
+
+## Author
+
+👤 **Okpara Tochukwu**
+
+- GitHub: [@xtrahuman](https://github.com/xtrahuman)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/tochukwu-okpara-449528197)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- src/components/interaction.js
+++ src/components/interaction.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Cone from '../images/Shape1.png'
+import Hand from '../images/Shape2.png'
+import Cart from '../images/Shape3.png'
 import { allData } from '../Redux/allData/reducer/reducer';
 
 const{web_pessimizer, mobile_pessimizer} = allData
@@ -11,7 +14,13 @@ const Interactions = ({bookingsCurrent, bookingsPrevious, str, averagePrice, ctr
 return (
   <div className='interaction-container'>
     <div className='d-flex section_style'>
-        <div className='section_width'>
+        <div className='section_width d-flex'>
+            <div>
+                <div className='big_round d-flex'>
+                <img src={Cone} alt="searchIcon" className="searchIcon" />
+                </div>
+            </div>
+            <div>
             <div className='percent_tag d-flex' >
             <p className='bolded_p desc_header'>Searches</p> <span className='percent_badge' style={{color:'#fff', backgroundColor: searchGreaterThan ? '#8BC34A' : '#FF6A67'}}>{searchPercent.toFixed(2)}%</span>
             </div>
@@ -22,6 +31,7 @@ return (
             <div className='style_prev flex_period'>
                 <span className='data_num'>{searchPrevious}</span>
                 <span className='data_str'>previous {period}</span>
+            </div>
             </div>
         </div>
         <div className='section_width'>
@@ -35,7 +45,13 @@ return (
         </div>
     </div>
     <div className='d-flex section_style'>
-        <div className='section_width'>
+    <div className='section_width d-flex'>
+            <div>
+                <div className='big_round d-flex'>
+                <img src={Hand} alt="searchIcon" className="searchIcon" />
+                </div>
+            </div>
+            <div>
             <div className='percent_tag d-flex' >
             <p className='bolded_p desc_header'>Clicks</p> <span className='percent_badge' style={{color:'#fff', backgroundColor:  clicksGreaterThan ? '#8BC34A' : '#FF6A67'}}>{ clicksPercent.toFixed(2)}%</span>
             </div>
@@ -47,6 +63,7 @@ return (
                 <span className='data_num'>{clicksPrevious}</span>
                 <span className='data_str'>previous {period}</span>
             </div>
+         </div>
         </div>
         <div className='section_width'>
             <p className='ctrColor bolded_p desc_header'>Ctr: {ctr?.toFixed(2)}%</p>
@@ -58,7 +75,13 @@ return (
         </div>
     </div>
     <div className='d-flex section_style'>
-    <div className='section_width'>
+    <div className='section_width d-flex'>
+            <div>
+                <div className='big_round d-flex'>
+                <img src={Cart} alt="searchIcon" className="searchIcon" />
+                </div>
+            </div>
+            <div>
             <div className='percent_tag d-flex' >
             <p  className='bolded_p desc_header'>Bookings</p> 
             </div>
@@ -70,9 +93,10 @@ return (
                 <span className='data_num'>{bookingsPrevious}</span>
                 <span className='data_str'>previous</span>
             </div>
+            </div>
         </div>
         <div className='section_width'>
-            <p  className='bolded_p desc_header'>Str: {str==null? `No data` : `${str?.toFixed(2)}%`}</p>
+            <p  className='bolded_p desc_header'>Str: {str === null? `No data` : `${str?.toFixed(2)}%`}</p>
             <p  className='bolded_p desc_header'>Avg.Price {averagePrice?.toFixed(2)}</p>
             <p className='faint_desc'>Conversion from cliks  to bookings on all devices.</p>
             <div>
